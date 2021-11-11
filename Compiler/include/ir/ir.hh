f56f16c22b3bbb750a12a7c9363d259658879676
chb <haobin_chen@mail.nankai.edu.cn>
2021-11-11 12:12:40 +0800

    IR

--- Compiler/include/ir/ir.hh
+++ Compiler/include/ir/ir.hh
@@ -28,6 +28,89 @@ typedef class IR {
 
 } IR;
 
+/**
+ * @brief Defines different operations.
+ * 
+ */
+typedef enum op_type {
+    NOP,
+    // Basic algorithmic operations
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+
+    // For floats.
+    FADD,
+    FSUB,
+    FMUL,
+    FDIV,
+    FNEG,
+
+    // Assignment
+    MOV,
+
+    // Function call
+    CALL,
+    RETURN,
+
+    // Jump
+    JMP,
+    JGE,
+    JLE,
+    JEQ,
+    JNE,
+    JLT,
+    JGT,
+
+    // Bit operations.
+    BIT_AND,
+    BIT_OR,
+    BIT_XOR,
+    BIT_NEG,
+    SHL,
+    LSHR,
+    ASHR,
+
+    // Logic operations.
+    LAND,
+    LOR,
+    LNOT,
+
+    // Relational operations.
+    CMP,
+
+    // Memory related operations.
+    LOAD,
+    STORE,
+    MALLOC
+
+    // Delimiters.
+    BEGIN_DATA,
+    END_DATA,
+    BEGIN_FUNC,
+    END_FUNC,
+    BEGIN_STRUCT,
+    END_STRUCT,
+
+    // Jump labels.
+    LBL,
+} op_type;
+
+/**
+ * @brief Defines different variable types.
+ * 
+ * i32 => 32-bit variables. int, unsigned int.
+ */
+typedef enum var_type {
+    i32,
+    i8,
+    // A little bit of complex...
+    DOUBLE,
+    FLOAT
+} var_type;
+
 } // namespace compiler::ir
 
 #endif
