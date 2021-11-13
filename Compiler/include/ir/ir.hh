dd984f4f7849947eccb7ad91595333811e428d90
Mark <etyyuiope@qq.com>
2021-11-13 22:28:19 +0800

    Added IR implementations

--- Compiler/include/ir/ir.hh
+++ Compiler/include/ir/ir.hh
@@ -17,17 +17,17 @@
 #ifndef IR_HH
 #define IR_HH
 
+#include <functional>
+#include <iostream>
+#include <string>
 #include <vector>
 
+namespace compiler {
+// Avoid circular includes.
+class Item;
+} // namespace compiler
+
 namespace compiler::ir {
-/**
- * @brief The class for IR (Intermediate Representation).
- * 
- */
-typedef class IR {
-
-} IR;
-
 /**
  * @brief Defines different operations.
  * 
@@ -110,6 +110,169 @@ typedef enum var_type {
     DB,
     FL,
 } var_type;
+
+/**
+ * @brief Base class for operand in the IR.
+ * 
+ */
+typedef class Operand {
+protected:
+    const var_type type;
+
+    const std::string identifier;
+
+    const bool is_ptr;
+
+    const bool is_var;
+
+    // We need to convert the value to hexical.
+    const std::string value;
+
+public:
+    Operand() = delete;
+
+    Operand(
+        const var_type& type,
+        const std::string& identifier,
+        const std::string& value,
+        const bool& is_var = true,
+        const bool& is_ptr = false);
+
+    virtual var_type get_type(void) const { return type; }
+
+    virtual bool get_is_ptr(void) const { return is_ptr; }
+
+    virtual bool get_is_var(void) const { return is_var; }
+
+    virtual std::string get_identifier(void) const { return identifier; }
+
+    virtual std::string get_value(void) const { return value; }
+
+    virtual ~Operand();
+} Operand;
+
+/**
+ * @brief Class for pointer types.
+ * 
+ */
+typedef class Operand_ptr final : public Operand {
+protected:
+    // How many aterisks it has.
+    const uint32_t shape;
+
+public:
+    Operand_ptr() = delete;
+
+    Operand_ptr(
+        const var_type& type,
+        const std::string& identifier,
+        const std::string& value,
+        const uint32_t& shape);
+} Operand_ptr;
+
+/**
+ * @brief The class for IR (Intermediate Representation). 
+ *        Some representation notations are taken from LLVM. @see https://llvm.org/docs/LangRef.html
+ * 
+ * Examples:
+ * ADD i32 foo, i8 bar, double baz
+ * ALLOC i32, align 8.
+ * 
+ * @note LOGIC EXPLAINED
+ *       When an IR instance is created from the AST, we should call \ref{compiler::ir::IR::generate_ir}
+ *       function to generate the string as follows:
+ *       1. Create a lambda function that can be passed to \ref{compiler::ir::IR::walk_ir}
+ *          that does some job with current operation;
+ *       2. The function \ref{compiler::ir::IR::walk_ir} will iterate through each operands,
+ *          and because each operand can be another instance of IR, we need to dig into the operand,
+ *          where callback function comes to help.
+ *       3. The callback will call each operand with callback again until it reaches a deepmost level:
+ *          the raw operand that we can ensure whether it is chained to the caller or not.
+ */
+typedef class IR final {
+private:
+    const op_type type;
+
+    const std::string label;
+
+    Operand* const operand_a;
+    Operand* const operand_b;
+    Operand* const operand_c;
+    Operand* const dst;
+
+    uint32_t lineno;
+
+    // void emit_helper(decltype<compiler::ir::isvar> callback, const bool& chained = true) const;
+
+    /**
+     * @brief Helper function for generating the IR. Since each variable / immediate value can be related to
+     *        some other IRs, we need a callback function that could correctly "notify" the caller whether the
+     *        source variable (src) should be written by the result (dst).
+     * 
+     * @param callback A callback function.
+     * @param chained 
+     * @return is_chained
+     */
+    bool emit_helper(std::function<bool(Operand* const)>&& callback, const bool& chained = true) const;
+    
+
+    /**
+     * @brief Call get_is_var().
+     * 
+     * @param callback 
+     * @param chained 
+     * @return is_chained
+     */
+    bool emit_helper(decltype(&Operand::get_is_var)&& callback, const bool& chained = true) const;
+
+    /**
+     * @brief The @ref{compiler::ir::IR::emit_ir} funciton will iterator over each operand and emit their IRs.
+     * 
+     * @param callback A lambda expression or a function object: the callback function.
+     * @param chained    Denote whether the result is related to another IR.
+     *                   Should be evaluated via callback.
+     */
+    void walk_ir(std::function<void(Operand* const)>&& callback, const bool& chained = true);
+
+public:
+    // Default constructor is definitely not allowed.
+    IR() = delete;
+
+    IR(
+        const op_type& operation,
+        Operand* const dst,
+        Operand* const operand_a,
+        Operand* const operand_b,
+        Operand* const operand_c,
+        const std::string& labal = "");
+
+    IR(
+        const op_type& operation,
+        Operand* const dst,
+        Operand* const operand_a,
+        Operand* const operand_b,
+        const std::string& labal = "");
+
+    IR(
+        const op_type& operation,
+        Operand* const dst,
+        Operand* const operand_a,
+        const std::string& label = "");
+
+    IR(
+        const op_type& operation,
+        Operand* const dst,
+        const std::string& label = "");
+
+    /**
+     * @brief Emit the IR.
+     * 
+     * @param item The current syntax tree node
+     * @param out 
+     * @param verbose 
+     */
+    void emit_ir(Item* const item, std::ostream& out = std::cout, const bool& verbose = false);
+} IR;
 
 } // namespace compiler::ir
 
--- Compiler/src/ir/ir.cc
+++ Compiler/src/ir/ir.cc
@@ -14,5 +14,132 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+#include <frontend/nodes/item.hh>
 #include <ir/ir.hh>
 
+compiler::ir::Operand::Operand(
+    const var_type& type,
+    const std::string& identifier,
+    const std::string& value,
+    const bool& is_var,
+    const bool& is_ptr)
+    : type(type)
+    , identifier(identifier)
+    , value(value)
+    , is_var(is_var)
+    , is_ptr(is_ptr)
+{
+}
+
+compiler::ir::Operand_ptr::Operand_ptr(
+    const var_type& type,
+    const std::string& identifier,
+    const std::string& value,
+    const uint32_t& shape)
+    : Operand(type, identifier, value, true)
+    , shape(shape)
+{
+}
+
+compiler::ir::IR::IR(
+    const op_type& operation,
+    Operand* const dst,
+    Operand* const operand_a,
+    Operand* const operand_b,
+    Operand* const operand_c,
+    const std::string& label)
+    : type(operation)
+    , dst(dst)
+    , operand_a(operand_a)
+    , operand_b(operand_b)
+    , operand_c(operand_c)
+    , label(label)
+{
+}
+
+compiler::ir::IR::IR(
+    const op_type& operation,
+    Operand* const dst,
+    Operand* const operand_a,
+    Operand* const operand_b,
+    const std::string& labal)
+    : type(operation)
+    , dst(dst)
+    , operand_a(operand_a)
+    , operand_b(operand_b)
+    , operand_c(nullptr)
+    , label(label)
+{
+}
+
+compiler::ir::IR::IR(
+    const op_type& operation,
+    Operand* const dst,
+    Operand* const operand_a,
+    const std::string& label = "")
+    : type(operation)
+    , dst(dst)
+    , operand_a(operand_a)
+    , operand_b(nullptr)
+    , operand_c(nullptr)
+    , label(label)
+{
+}
+
+compiler::ir::IR::IR(
+    const op_type& operation,
+    Operand* const dst,
+    const std::string& label = "")
+    : type(operation)
+    , dst(dst)
+    , operand_a(nullptr)
+    , operand_b(nullptr)
+    , operand_c(nullptr)
+    , label(label)
+{
+}
+
+void compiler::ir::IR::emit_ir(Item* const item, std::ostream& output, const bool& verbose)
+{
+    lineno = item->get_lineno();
+
+    // Emit IR of each operand.
+    auto lambda_walk_ir = [&](Operand* const operand) {
+        if (operand == nullptr) {
+            output << "\t" << label;
+            return;
+        }
+
+        const bool is_var = operand->get_is_var();
+        output << (is_var == true ? operand->get_identifier() : operand->get_value())
+               << "\t" << label << std::endl;
+    };
+
+    walk_ir(lambda_walk_ir, true);
+}
+
+void compiler::ir::IR::walk_ir(std::function<void(Operand* const)>&& callback, const bool& chained)
+{
+    emit_helper([&](Operand* const operand) -> bool {
+        callback(operand);
+        return false;
+    },
+        chained);
+}
+
+bool compiler::ir::IR::emit_helper(
+    decltype(&Operand::get_is_var)&& callback,
+    const bool& chained) const
+{
+    return emit_helper([&](Operand* const operand) -> bool {
+        return (operand->*callback)();
+    },
+        chained);
+}
+
+bool compiler::ir::IR::emit_helper(
+    std::function<bool(Operand* const operand)>&& callback,
+    const bool& chained = true) const
+{
+    return (chained && callback(dst)) || callback(operand_a) || callback(operand_b) || callback(operand_c);
+}
